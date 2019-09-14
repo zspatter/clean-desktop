27d155b0228493b66effa58cb7387203171ccae9
zspatter <localhost@zspatter.dev>
2019-09-14 19:42:53 -0400

    Alters watch/destination paths

--- clean_desktop/desktop_cleaner.py
+++ clean_desktop/desktop_cleaner.py
@@ -6,8 +6,8 @@ from watchdog.observers import Observer
 from clean_desktop.EventHandler import EventHandler
 
 if __name__ == '__main__':
-    watch_path = Path('/Users/zspatter/Desktop/test')
-    destination_root = Path('/Users/zspatter/Desktop/zspatter')
+    watch_path = Path('/Users/zspatter/Desktop')
+    destination_root = Path('/Users/zspatter/Desktop/holder of things')
     event_handler = EventHandler(watch_path=watch_path, destination_root=destination_root)
     observer = Observer()
     observer.schedule(event_handler, f'{watch_path}', recursive=True)
